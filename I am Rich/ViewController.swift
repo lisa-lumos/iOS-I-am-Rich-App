8b80d968f96e3f98dccb21cb78d713fe136389e1
Lisa <84500263+lisajia307@users.noreply.github.com>
2021-08-20 02:09:48 -0600

    Add files via upload

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by lisa on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
